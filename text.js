794f1429df450f01e7c98478fab82506f52cfbd2
Constantin Bierschenk <Constantin_Bierschenk@live.com>
2024-01-23 18:01:15 -0800

    Webpage text adjustments
    Changes to be committed:
    	modified:   text.js

--- text.js
+++ text.js
@@ -4,31 +4,25 @@ text = {
       text: [
         "<h2>Constantin</h2>",
         "German electrical engineer with a high interest in Software Development and Circuit Design. Currently located in Canada to gain international experience as a Software Developer in the financial field. Further know-how obtained in the past:",
-        "<p>- Software Development in Automotive</p>",
-        "<p>- DevOps</p>",
-        "<p>- Circuit Design in Aviation</p>",
+        "<p><h4>- Software development in automotive</h4></p>",
+        "<p><h4>- DevOps</h4></p>",
+        "<p><h4>- Circuit design in aviation</h4></p>",
       ],
       bootstrap: "bi bi-person-bounding-box",
     },
     travel: {
       text: [
         "<h2>Globetrotter</h2>",
-        "<p>The world is big, the world is beautiful! Therefore, I developed the habit to travel to different places whenever enough time exists. There is so much to explore and learn from all the interesting cultures and awesome people. I just can encourage everyone to step over their shadow and go out and explore the planet. For more information about my small adventures, visit my blog (coming soon).</p><br><br>",
-        '<p style="text-align:center;">“The purpose of life is to live it, to taste experience to the utmost, to reach out eagerly and without fear for newer and richer experience.” - Eleanor Roosevelt</c> </p>',
+        "<p>The world is big, the world is beautiful! Therefore, I developed the habit to travel to different places whenever enough time exists. There is so much to explore and learn from all the interesting cultures and awesome people. I just can encourage everyone to step over their shadow and go out and explore the planet.</p>",
       ],
       bootstrap: "bi bi-airplane",
     },
-  },
-  archivements: {
-    1: "1",
-    2: "2",
-    3: "3",
   },
   projects: {
     0: {
       title: "Portfolio webpage",
       description:
-        "<p>How to learn new tools for your knowledge toolbox? Yeah, right, by reading and getting used to the tool. The best way to get used to something is by playing around with the learned knowledge. Thus, this project based on exactly that idea. I created my webpage portfolio's first version via HTML, CSS and vanilla JavaScript as a starting point. Because first mastering the basics and later master the frameworks like React etc.</p>",
+        "<p>How to learn new tools? Yeah, right, by reading and getting used to the tool. The best way to get used to something is by playing around with the learned knowledge. Thus, this project based on exactly that idea. I created my webpage's first version using HTML, CSS and vanilla JavaScript as a starting point. First mastering the basics and later the frameworks like React etc.</p>",
       img: "./dummy.jpeg",
     },
   },
